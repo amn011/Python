4c6de122f847218effbb14f3b51f636c26d6bc9d
Aman <amanmuzaffarpur11@gmail.com>
2021-05-29 17:18:49 +0530

    first commit

---
+++ if else.py
@@ -0,0 +1,10 @@
+
+a=int(input("enter 1st number\n"))
+b=int(input("enter 2nd number\n"))
+c=(a+b)
+print("sum is ",c)
+
+if c>=10:
+ print('it is two digit number')
+else:
+ print('it is single digit number')
