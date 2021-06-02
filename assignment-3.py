0678dbbb5093aa934ff9b5d46bc23eaf37193e87
Aman <amanmuzaffarpur11@gmail.com>
2021-06-02 20:34:21 +0530

    first commit

---
+++ assignment-3.py
@@ -0,0 +1,14 @@
+# print marks in sorted form entered by user:
+
+
+m1=int(input("enter marks of stdent 1 :"))
+m2=int(input("enter marks of stdent 2 :"))
+m3=int(input("enter marks of stdent 3 :"))
+m4=int(input("enter marks of stdent 4 :"))
+m5=int(input("enter marks of stdent 5 :"))
+
+marksLIST=[ m1 , m2 , m3 , m4 , m5 ]
+marksLIST.sort()
+print(marksLIST)
+
+ 
