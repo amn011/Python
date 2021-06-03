cf4b55eb44a7e0dd1c8cd4ac718e1c14e7379156
Aman <amanmuzaffarpur11@gmail.com>
2021-06-03 12:59:37 +0530

    first commit

---
+++ 41Dictionary.py
@@ -0,0 +1,9 @@
+myDICT= {
+"name": "Aman",
+"course": "B.tech",
+"marks": "[64, 74, 89]",
+}
+
+print(myDICT['name'])
+print(myDICT['course'])
+print(myDICT['marks'])
---
+++ 42dictionary-methods.py
@@ -0,0 +1,23 @@
+myDICT= {
+"name": "Aman",
+"course": "B.tech",
+"marks": "[64, 74, 89]",
+1:2
+}
+
+#dictionary methods:
+
+
+print(myDICT.values())   #print the values from dictionary
+print(myDICT.items())    #print the all items(key,value) from dictionary
+
+updateDICT={
+"name": "Kumar"
+}
+
+myDICT.update(updateDICT)  #update the dictionary
+print(myDICT)
+
+print(myDICT.get("name"))
+
+
