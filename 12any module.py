c80e1ca89d8cce2610272dcae0e18a36eebe5176
Aman <amanmuzaffarpur11@gmail.com>
2021-06-10 18:15:24 +0530

    Update 12any module.py

--- 12any module.py
+++ 12any module.py
@@ -1,2 +1,2 @@
 from playsound import playsound
-playsound('D:\\xyz\\xyz.mp3')   #insert path of mp3 file of in your local storage
+playsound('D:\\abc\\xyz.mp3')   #insert path of mp3 file of in your local storage
