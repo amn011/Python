2d34f9563711d126a3f2fd2588d250008412613c
Aman <amanmuzaffarpur11@gmail.com>
2021-06-10 18:13:22 +0530

    34tupples.py

--- 34tupples.py
+++ 34tupples.py
@@ -1,11 +1,11 @@
 # tupples are like lists but it cannot  be altered or changed or updated
 
 
-t=[1,51,9,4,10,3,]
+t=[1,51,8,4,10,3,]
 print(t)
 
 #tuple methods:
-print(t.count(9))  #----> this will count no of 9 in list
-print(t.index(9))  #----> this will let us know the index no. of 9 in the list
+print(t.count(8))  #----> this will count no of 9 in list
+print(t.index(8))  #----> this will let us know the index no. of 9 in the list
 
 
