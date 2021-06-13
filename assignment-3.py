b3743880b2ef2fc574bf7a7b686bac3854b561f6
Aman <amanmuzaffarpur11@gmail.com>
2021-06-13 20:59:18 +0530

    update

--- assignment-3.py
+++ assignment-3.py
@@ -5,9 +5,8 @@ m1=int(input("enter marks of stdent 1 :"))
 m2=int(input("enter marks of stdent 2 :"))
 m3=int(input("enter marks of stdent 3 :"))
 m4=int(input("enter marks of stdent 4 :"))
-m5=int(input("enter marks of stdent 5 :"))
 
-marksList=[ m1 , m2 , m3 , m4 , m5 ]
+marksList=[ m1 , m2 , m3 , m4 ]
 marksList.sort()
 print(marksList)
 
