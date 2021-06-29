c8ac92f4f43c0297a0c2d93da6602ded141174f6
Aman <amanmuzaffarpur11@gmail.com>
2021-06-29 21:17:37 +0530

    first commit

---
+++ assignment-71.py
@@ -0,0 +1,12 @@
+#program to convert celsius to fahrenheit:
+
+
+
+def fahr(cel):
+    return (cel*(9/5))+32
+c=float(input("enter temperature in celcius :"))
+f= fahr(c)
+print("temperature in fehrenheit is ",+f)
+
+
+
