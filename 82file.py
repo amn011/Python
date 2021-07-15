ee41b8012228afbec18a896d790f3864450001d9
Aman <amanmuzaffarpur11@gmail.com>
2021-07-15 21:20:53 +0530

    first commit

---
+++ 82file.py
@@ -0,0 +1,22 @@
+from os import close
+
+
+f=open ('sample.txt', 'r')
+
+#read first line:
+text=f.readline()
+print(text)
+
+#read 2nd line:
+text=f.readline()
+print(text)
+
+
+#read 3rd line:
+text=f.readline()
+print(text)
+
+
+#read 4th line:
+text=f.readline()
+print(text)
