ec820344da02b27e0d93d8109bfc690a24da382c
Aman <amanmuzaffarpur11@gmail.com>
2021-07-16 10:54:54 +0530

    first commit

---
+++ 84file_append.py
@@ -0,0 +1,3 @@
+f=open('sample2.txt', 'a')             #'a' will append the text that will be written in write section       
+f.write ("this is another sample file that i am appending") 
+f.close()
